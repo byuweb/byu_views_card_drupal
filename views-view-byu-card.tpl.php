6a1e9ba8b981d01415962f9330c2ad99be0f6a11
katrialesser <katrialynn18@gmail.com>
2017-02-14 09:58:46 -0700

    intially commiting views card module files

---
+++ views-view-byu-card.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Default simple view template to display a rows in a BYU card.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ * - $columns contains a nested array of columns. Each column contains an
+ *   array of columns.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)) : ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+
+<div class="card-container" class="<?php print $classes; ?>">
+    <?php foreach ($rows as $id => $row): ?>
+        <byu-card><?php print $row; ?></byu-card>
+    <?php endforeach; ?>
+
+</div>
+
