40629eafc9d634c20c4550d745715ce84401f030
katrialesser <katrialynn18@gmail.com>
2017-02-24 14:19:43 -0700

    moving the class to the container

--- views-view-byu-card.tpl.php
+++ views-view-byu-card.tpl.php
@@ -15,9 +15,8 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 
-<div class="card-container" >
+<div class="card-container <?php print $card_width[$id]; ?>" >
     <?php foreach ($rows as $id => $row): ?>
-        <byu-card columns="<?php print $cols[$id]; ?>" class="<?php print $card_width[$id]; ?>"><?php print $row; ?></byu-card>
     <?php endforeach; ?>
 
 </div>
